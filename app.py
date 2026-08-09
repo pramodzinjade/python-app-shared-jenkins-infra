783b2ff4255ce9ac5f6e9f5b4d677418e950b44b
pramodzinjade <pramodzinjade73@gmail.com>
2026-08-09 20:02:07 +0530

    modifies

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
 import os
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_geek():
-    return 'hii from pramod zinjade !!!!!!!!!, added webhook'
-@app.route('/hi')
-def hell():
-    return '<h1>Hiiiiiiii from Flask & Docker</h1>'
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/hi")
+def hi():
+    return "Hiiiiiiii from Flask & Docker"
+
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
